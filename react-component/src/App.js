aa6594d4d409c6f32ac4dd75641a69fe33729312
Zhong Weibin <weibin.zhong@maxent-inc.com>
2018-03-20 18:15:37 +0800

    strategy tree

--- react-component/src/App.js
+++ react-component/src/App.js
@@ -13,9 +13,6 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">react 自定义组件测试</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
         {/*策略树*/}
         <StrategyTreeEX />
